bcfb70d300f19d785d51d527161997a7c67ad6b6
gldsct <188839581+gldsct@users.noreply.github.com>
2024-11-26 16:14:33 +0545

    Add GUI for project

--- javascript.js
+++ javascript.js
@@ -1,100 +1,74 @@
-function getComputerChoice(){
-    let choice = (Math.floor(Math.random() * 3));
-    if (choice === 0) {
-        return "rock";
-    }
-    else if (choice === 1) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-
-function getHumanChoice(){
-    let choice = prompt("Pick rock, paper or scissors.");
-    if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors") {
-        return choice.toLowerCase();
-    }
-    else {
-        alert("Your only options are ROCK, PAPER or SCISSORS. Try again!")
-        return getHumanChoice();
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
 }
 
-function playRound() {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-
-    if (humanChoice === computerChoice) {
-        console.log(`You picked ${humanChoice} and the computer also picked ${computerChoice}. It was a tie!`);
-    }
-    else {
-        if (humanChoice === "rock") {
-            switch (computerChoice) {
-                case "paper":
-                    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You lost!`);
-                    computerScore += 1;
-                    break;
-                case "scissors":
-                    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You won!`);
-                    humanScore += 1;
-                    break;
-            }
-        }
-        else if (humanChoice === "paper") {
-            switch (computerChoice) {
-                case "rock":
-                    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You won!`);
-                    humanScore += 1;
-                    break;
-                case "scissors":
-                    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You lost!`);
-                    computerScore += 1;
-                    break;
-            }
-        }
-        else if (humanChoice === "scissors") {
-            switch (computerChoice) {
-                case "rock":
-                    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You lost!`);
-                    computerScore += 1;
-                    break;
-                case "paper":
-                    console.log(`You picked ${humanChoice}. Computer picked ${computerChoice}. You won!`);
-                    humanScore += 1;
-                    break;
-            }
-        }
-    }
-}
-
-function playGame() {
-    if (roundsPlayed < 5) {
-        roundsPlayed += 1;
-        console.log(`Round ${roundsPlayed}!`);
-        playRound();
-        playGame();
-    }
-    else {
-        console.log(`5 rounds completed. Game over.\nFinal Score\nHuman: ${humanScore} / Computer: ${computerScore}`);
-        if (humanScore === computerScore) {
-            console.log("No one won. It was a tie!");
+function playRound (playerSelection, computerSelection) {
+    if (playerScore < 5 && computerScore < 5) {
+        if (playerSelection === computerSelection) {
+            roundResult.textContent += `${playerSelection[0].toUpperCase() + playerSelection.slice(1)} ties with ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}.\n`;
         }
         else {
-            if (humanScore > computerScore) {
-                console.log("You won! Congratulations!");
+            if ((playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper")) {
+                roundResult.textContent += `${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}. You win this round!\n`;
+                playerScore += 1;
             }
-            else {
-                console.log("The computer won. Better luck next time!");
-            }
+            else if ((playerSelection === "rock" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "scissors") ||
+            (playerSelection === "scissors" && computerSelection === "rock")) {
+                roundResult.textContent += `${playerSelection[0].toUpperCase() + playerSelection.slice(1)} gets beaten by ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}. You lose this round!\n`;
+                computerScore += 1;
+            }   
         }
+        gameResults (playerScore, computerScore);
     }
 }
 
-let humanScore = 0;
+function gameResults (playerScore, computerScore) {
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5) gameResult.textContent = `Congratulations! You won the game!!!`;
+        else gameResult.textContent = `Sorry! You lost the game!!!`;
+        resetGame ();
+        }
+}
+
+function resetGame () {
+    result.appendChild(newGameButton);
+    newGameButton.addEventListener("click", (event) => {
+        playerScore = 0;
+        computerScore = 0;
+        roundResult.textContent = "";
+        gameResult.textContent = "";
+        result.removeChild(newGameButton);
+    });
+}
+
+let playerScore = 0;
 let computerScore = 0;
 
-let roundsPlayed = 0;
+const allChoices = document.querySelectorAll("button");
 
-playGame();
+allChoices.forEach((choice) => {
+    choice.addEventListener("click", (event) => {
+        playRound(choice.id, getComputerChoice());
+    });
+});
+
+// 
+
+const result = document.querySelector("#result");
+const roundResult = document.querySelector("#roundResult");
+const gameResult = document.querySelector("#gameResult");
+
+const newGameButton = document.createElement("button");
+newGameButton.textContent = "Start new game?"
+newGameButton.style.cssText = "height: 30px; width: 200px; font-size: 22px;"
